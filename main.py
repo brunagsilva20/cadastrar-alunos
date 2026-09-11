6785bb7d734fe2ef501b616e59f4d90334b54aab
Bruna Gabriela <bruna.g.silva20@aluno.senai.brgit kqmfiqn\1>
2026-09-11 10:57:02 -0300

    feat: adiciona cadastro de alunos

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 alunos = []
-while True:
-    print("\n--- CADASTRO DE ALUNOS ---")
-    print("0 - Sair")
-    opcao = input("Escolha: ")
-    if opcao == "0":
-        print("Programa encerrado.")
-        break
-    else:
-        print("Opção inválida.")
+def cadastrar():
+    nome = input("Nome do aluno: ")
+    alunos.append(nome)
+    print("Aluno cadastrado.")
+    while True:
+        print("\n--- CADASTRO DE ALUNOS ---")
+        print("1 - Cadastrar")
+        print("0 - Sair")
+        opcao = input("Escolha: ")
+
+        if opcao == "0":
+            print("Programa encerrado.")
+            break
+        elif opcao == "1":
+            cadastrar()
+        else:
+            print("Opção inválida.")
