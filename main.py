e60a3bfc0fdf8be193faa9727156314887b9e4a3
Bruna Gabriela <bruna.g.silva20@aluno.senai.brgit kqmfiqn\1>
2026-09-11 10:22:26 -0300

    feat:Cria menu inicial do cadastro

---
+++ main.py
@@ -0,0 +1,10 @@
+alunos = []
+while True:
+    print("\n--- CADASTRO DE ALUNOS ---")
+    print("0 - Sair")
+    opcao = input("Escolha: ")
+    if opcao == "0":
+        print("Programa encerrado.")
+        break
+    else:
+        print("Opção inválida.")
