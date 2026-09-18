0e0c58e1d2785faabbdc748851be2626fe9d969d
Bruna Gabriela <bruna.g.silva20@aluno.senai.brgit kqmfiqn\1>
2026-09-18 08:57:49 -0300

    feat:adiciona busca por nome completo

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ def cadastrar():
             return
     alunos.append(nome)
     print("Aluno cadastrado.")
-
 def listar():
     if len(alunos) == 0:
         print("Nenhum aluno cadastrado.")
@@ -18,11 +17,18 @@ def listar():
     print("\n--- ALUNOS ---")
     for numero, aluno in enumerate(alunos, start=1):
         print(f"{numero}. {aluno}")
-        print(f"Total: {len(alunos)} aluno(s)")
+    print(f"Total: {len(alunos)} aluno(s)")
+def buscar():
+    termo = input("Nome completo para buscar: ").strip()
+    for aluno in alunos:
+        if aluno.lower() == termo.lower():
+            print(f"Encontrado: {aluno}")
+            return
 while True:
     print("\n--- CADASTRO DE ALUNOS ---")
     print("1 - Cadastrar")
     print("2 - Listar")
+    print("3 - Buscar")
     print("0 - Sair")
     opcao = input("Escolha: ")
     if opcao == "0":
@@ -32,6 +38,7 @@ while True:
         cadastrar()
     elif opcao == "2":
         listar()
-
+    elif opcao == "3":
+        buscar()
     else:
         print("Opção inválida.")
